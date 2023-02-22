d2dfb67b64a7823f1c0a8900d4f00caf3c2f778e
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-02-21 19:40:05 -0500

    crear nuevo proyecto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import validator from './validator.js';
+var a = validator.isValid("5191732343975393")
+var b = validator.maskify("") 
+const card = document.getElementById ("name");
+const inicioDiv = document.getElementById("inicio");
+const enviar = document.getElementById ("enviar");
+
+btn enviar.addEventListener ("click" , ()) => { 
+
+}
---
+++ src/validator.js
@@ -0,0 +1,30 @@
+const validator = {
+  isValid : (creditCardNumber) => {
+    let total = 0;
+    const creditCardReverse = [...creditCardNumber].reverse();
+    for (let p = 0; p < creditCardReverse.length; p++) {
+      if (p%2 != 0 ){
+        var valuePositionResult = creditCardReverse[p] * 2; 
+        if (valuePositionResult >= 10){
+          valuePositionResult = String(valuePositionResult)
+          valuePositionResult = Number(valuePositionResult[0])+Number(valuePositionResult[1]);
+        };
+        total = total + valuePositionResult;
+      } else {
+        total = total +Number(creditCardReverse[p]);
+      };
+    };
+    if(total%10 === 0){
+      return true;
+    }
+    else return false;
+  },
+  maskify : (creditCardNumber) => {
+      if (creditCardNumber.length < 4) return creditCardNumber
+      var cantidadDigitos = creditCardNumber.length -4;
+      const last4Characters = creditCardNumber.substr(-4); 
+      const maskingCharacters = '*'.repeat(cantidadDigitos) + last4Characters;
+      return (maskingCharacters);
+}
+}
+export default validator;
