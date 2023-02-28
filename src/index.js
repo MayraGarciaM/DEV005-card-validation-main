746bc9489c7228fe0330e1d419a8b3d029d705be
MayraGarciaM <stefagarcia.23@hotmail.com>
2023-02-28 09:30:43 -0500

    agregar comentarios

--- src/index.js
+++ src/index.js
@@ -1,29 +1,54 @@
 import validator from './validator.js';
+// Se toma el elemento numero-tarjeta por el Id
 const inputNumeroTarjeta = document.getElementById('numero-tarjeta');
+
+// Se agrega un evento listener tipo keypress al input de numero-tarjeta
 inputNumeroTarjeta.addEventListener('keypress', (e) => {
-  if (!soloNumeros(event)) e.preventDefault();
+   // Se valida que la tecla presionada sean correspondientes a la tecla con el cod asignado a numero 	
+  if (!soloNumeros(e)) e.preventDefault(); // e.preventDefault es para continuar el flujo normal
 })
+
+// La funcion solo numeros validara que la key de la tecla este entre 48 y 57 "solo los numeros"
 function soloNumeros(e) {
-	const key = e.charCode;
+	const key = e.charCode; // charCode es el cod de la tecla
 	return key >= 48 && key <= 57;
 }
+
+// Tomamos el elemento btn por el id validar-tarjeta
 const btnValidarTarjeta = document.getElementById("validar-tarjeta");
+
+// Se agrega un evento listener de tipo click al btn
 btnValidarTarjeta.addEventListener("click", () => {
-	const numeroTarjeta = inputNumeroTarjeta.value;
+
+	const numeroTarjeta = inputNumeroTarjeta.value; // Tomamos el numero de tarjeta desde el input numero-tarjeta
+
+	// Validamos que el numero de tarjeta no este vacio o que tenga 16 caracteres
 	if (numeroTarjeta !== "" || numeroTarjeta.length === 16) {
-		document.getElementById("n-tarjeta").innerHTML = validator.maskify(numeroTarjeta);
+
+		document.getElementById("n-tarjeta").innerHTML = validator.maskify(numeroTarjeta); //Usamos la funcion maskyfy para mostrar los ultimos 4 dig de la tj
+
+		//Validamos con la funcion isValid el numero de tc
 		if (validator.isValid(numeroTarjeta)) {
-			document.getElementById("mensaje-valido").style.display = "block";
-			inputNumeroTarjeta.value = "";
+
+			// Tomamos un elemento por el Id mensaje-valido y en la propiedad de estilo display se la cambiamos a block
+			document.getElementById("mensaje-valido").style.display = "block"; 
+			inputNumeroTarjeta.value = ""; // Eliminamos el numero de tarjeta del input (limpiar el formulario)
 		}
 		else {
+
+			// Tomamos un elemento por el ID mensaje-erroneo y en la propiedad de estilo display la camabiamos a block
 			document.getElementById("mensaje-erroneo").style.display = "block";
-			document.getElementById("mensaje-erroneo").innerHTML = "Tu tarjeta es invalida!";
+
+		    // Tomamos un elemento por el Id mensaje-erroneo y con la propiedad .innerHTML ingresamos un texto
+			document.getElementById("mensaje-erroneo").innerHTML = "Tu tarjeta es invalida!"; 
 		}
 	} else {
+		// Tomamos un elemento por el ID mensaje-erroneo y en la propiedad de estilo display la camabiamos a block
 		document.getElementById("mensaje-erroneo").style.display = "block";
+		// Tomamos un elemento por el Id mensaje-erroneo y con la propiedad .innerHTML ingresamos un texto
 		document.getElementById("mensaje-erroneo").innerHTML = "Porfavor complete el numero de tarjeta!";
 	}
+	// Agregamos un setTimeout con un tiempo de 5 seg para que oculte los msj
 	setTimeout(() => {
 		document.getElementById("mensaje-valido").style.display = "none";
 		document.getElementById("mensaje-erroneo").style.display = "none";
--- src/validator.js
+++ src/validator.js
@@ -1,30 +1,44 @@
 const validator = {
+	//Se crea una funcion flecha que va a recibir creditCardNumber y va a retornar si es valido o no
 	isValid: (creditCardNumber) => {
-		let total = 0;
-		const creditCardReverse = [...creditCardNumber].reverse();
+		let total = 0; // Creamos var total que contiene la suma de todo el algoritmo
+		const creditCardReverse = [...creditCardNumber].reverse(); // reversamos el num de tc para que quede al reves con la funcion reverse
+		
+		//Recorre todos los num de la tc se indica la posicion inicial en este caso 0 y va aumentando prgresivamente en la cant de digitos de la tc
 		for (let p = 0; p < creditCardReverse.length; p++) {
-			if (p % 2 !== 0) {
-				let valuePositionResult = creditCardReverse[p] * 2;
+
+			//Se valida si la posicion es par o impar con el modulo de la division 
+			//Se valida las posciiones impares porque en JS las posiciones inician desde 0 y no desde 1
+			if (p % 2 !== 0) { 
+
+				let valuePositionResult = creditCardReverse[p] * 2; //Se multiplica el valor de la posicion p por 2
+				//se valida si el num es mayor a 10
+				//segun el algoritmo de luhn si la multi es igual a 10 se suman sus digitos
 				if (valuePositionResult >= 10) {
 					valuePositionResult = String(valuePositionResult);
 					valuePositionResult = Number(valuePositionResult[0]) + Number(valuePositionResult[1]);
 				}
+				//Se suma el valor de total mas el resultado final
 				total = total + valuePositionResult;
 			} else {
+				//Si la posicion no es impar solo se suma el numero de la posicion p
 				total = total + Number(creditCardReverse[p]);
 			}
 		}
+		//segun el algorimo el mod de la division entre total y 10 debe ser igual a cero, si es asi la tj es valida
 		if (total % 10 === 0) {
 			return true;
 		}
+		//si el modulo del total y 10 es diferente a 0 la tj es invalida
 		else return false;
 	},
 	maskify: (creditCardNumber) => {
-		if (creditCardNumber.length < 4) return creditCardNumber;
+		if (creditCardNumber.length < 4) return creditCardNumber; // Si el numero de la tarjeta tiene menos de 4 digitos no es necesario realizar el maskify
+		//Guardamos la cant. de digitos que tiene y le restamos 4 que va a ser el espacio de los ulti 4 num
 		const cantidadDigitos = creditCardNumber.length - 4;
-		const last4Characters = creditCardNumber.substr(-4);
-		const maskingCharacters = '*'.repeat(cantidadDigitos) + last4Characters;
-		return (maskingCharacters);
+		const last4Characters = creditCardNumber.substr(-4); // Con la funcion substr tomamos los ult 4 digitos de la tj
+		const maskingCharacters = '*'.repeat(cantidadDigitos) + last4Characters; // con la funcion repeat, repetimos el * n cantidad de veces y le concatenamos los ultimos 4 dig
+		return (maskingCharacters); //Retornamos el num de la tj formateada
 	}
 }
 export default validator;
